779f83ec2a798e459727e38a68b002a8ba843253
Neuneu <neufae@yahoo.com>
2024-12-09 14:34:40 -0500

    Add project files.

---
+++ numeroUNO/Program.cs
@@ -0,0 +1,28 @@
+﻿
+var stream = File.Open("C:\\Users\\neufa\\repos\\numeroUNO\\numeroUNO\\generatedCode.txt", FileMode.Open);
+string text = string.Empty;
+using(var reader = new StreamReader(stream))
+{
+    text = reader.ReadToEnd();
+}
+var lines = text.Split(Environment.NewLine).ToList();
+var leftList = new List<string>();
+var rightList = new List<string>();
+lines.ForEach(line =>
+{
+    var split = line.Split("   ").ToList();
+    leftList.Add(split[0]);
+    rightList.Add(split[1]);
+});
+leftList.Sort();
+rightList.Sort();
+
+var diffList = new List<int>();
+for (int i = 0; i < leftList.Count; i++)
+{
+    int left = int.Parse(leftList[i]);
+    int right = int.Parse(rightList[i]);
+    diffList.Add(Math.Abs(left - right));
+}
+var sum = diffList.Sum();
+int debug = 0;
